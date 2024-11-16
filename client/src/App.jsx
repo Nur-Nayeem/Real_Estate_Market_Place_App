5c34342951b25a3656ebdf65913f41a9fc4da504
Nur-Nayeem <nurnayem768@gmail.com>
2024-11-16 21:18:09 +0600

    Real Estate Market Place App

---
+++ client/src/App.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter,Routes,Route } from "react-router-dom";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import Home from "./pages/home";
+import Header from "./Components/Header";
+import PrivateRoute from "./Components/PrivateRoute";
+
+export default function App() {
+    return (
+    <BrowserRouter>
+        <Header/>
+        <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/sign-in" element={<Signin/>}/>
+            <Route path="/sign-up" element={<Signup/>}/>
+            <Route path="/about" element={<About/>}/>
+            <Route element={<PrivateRoute/>}>
+                <Route path="/profile" element={<Profile/>}/>
+            </Route>
+        </Routes>
+    </BrowserRouter>)
+}
